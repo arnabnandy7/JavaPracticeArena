d1a6606258881643d6a7b17bc235949bc3f5fa71
Arnab Nandy <arnab_nandy7@yahoo.com>
2020-09-27 21:22:13 +0530

    Project Arena structure changed

---
+++ practise/lambdaExpr/LamdaEventHandler.java
@@ -0,0 +1,32 @@
+//package practise.lambdaExpr;
+//
+//import javafx.scene.*;
+//import javafx.scene.control.cell.*;
+//import javafx.scene.layout.*;
+//import javafx.stage.*;
+//import javafx.application.*;
+//
+//public class LamdaEventHandler extends Application {
+//
+////	@Override
+////	public void start(Stage primaryStage) throws Exception {
+////		Button btn = new Button();
+////		btn.setText("Say 'Hello World'");
+////
+////		btn.setOnAction(event -> System.out.println("Hello World!"));
+////
+////		StackPane root = new StackPane();
+////		root.getChildren().add(btn);
+////
+////		Scene scene = new Scene(root, 300, 250);
+////
+////		primaryStage.setTitle("Hello World!");
+////		primaryStage.setScene(scene);
+////		primaryStage.show();
+////	}
+//
+//	public static void main(String args[]) {
+//		//launch(args);
+//	}
+//
+//}
