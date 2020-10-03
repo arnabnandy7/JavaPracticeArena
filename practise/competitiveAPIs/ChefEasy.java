bd6e3a1e017c85b99ca0bf85aa1049f22ab876b5
Arnab Nandy <arnab_nandy7@yahoo.com>
2020-10-03 22:53:37 +0530

    CodeChef Practise

---
+++ practise/competitiveAPIs/ChefEasy.java
@@ -0,0 +1,35 @@
+package practise.CompetitiveAPIs;
+
+import java.util.Scanner;
+
+public class ChefEasy {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		long T;
+		T=sc.nextLong();
+		long x=0;
+		while(x<T){
+			long count=0;
+			int n=sc.nextInt();
+			int k=sc.nextInt();
+			long[] q=new long[n];
+			for(int i=0;i<n;i++){
+				q[i]=sc.nextLong();
+			}
+			int i;
+			for(i=0;i<n;i++){
+				if(q[i]>=k){
+					count++;
+					q[i+1]+=(q[i]-k);
+				}
+				if(q[i]<k)
+					break;
+			}
+			if(i==n)
+				count+=((q[n-1]-k)/k);
+			System.out.println(count+1);
+			x++;
+		}
+		sc.close();
+	}
+}
---
+++ practise/competitiveAPIs/ReplaceForX.java
@@ -0,0 +1,46 @@
+package practise.CompetitiveAPIs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ReplaceForX {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int T=sc.nextInt();
+		
+		if(T>=1 && T<=100){
+			int sumOfN=0;
+			for(int i=0;i<T;i++){
+				int N=sc.nextInt();
+				sumOfN+=N;
+				int X=sc.nextInt();
+				int p=sc.nextInt();
+				int k=sc.nextInt();
+				if(p>=1 && p<=N && k>=1 && k<=N && X>=0 && X<=1000000000){
+					ArrayList<Integer> aI=new ArrayList<Integer>();
+					for(int j=0;j<N;j++){
+						int tempVar=sc.nextInt();
+						if(tempVar>=0 && tempVar<=1000000000){
+							aI.add(tempVar);
+						}
+					}
+					Collections.sort(aI);
+					if(aI.indexOf(X)==-1)
+						System.out.println(aI.indexOf(X));
+					else
+					{
+						int cur=aI.indexOf(X)+1;
+						if(cur==p){
+							System.out.println(0);
+						}else{
+							System.out.println(Math.abs(cur-p));
+						}
+					}
+				}
+			}
+		}
+		sc.close();
+	}
+}
