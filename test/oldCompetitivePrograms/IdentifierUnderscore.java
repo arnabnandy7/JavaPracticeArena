22617ffd9f72ef45c2b1b480df28e468e3b574da
Arnab Nandy <arnab_nandy7@yahoo.com>
2021-04-01 22:46:06 +0530

    Removed unused variables

--- test/oldCompetitivePrograms/IdentifierUnderscore.java
+++ test/oldCompetitivePrograms/IdentifierUnderscore.java
@@ -1,10 +1,9 @@
 package test.oldCompetitivePrograms;
 
 public class IdentifierUnderscore {
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		int _=0;
-		int __ = _+1;
+		//int _=0;
+		//int __ = _+1;
 		
 	}
 }
